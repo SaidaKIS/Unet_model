497a32e20a12ae48361a322b36f09b5eeeb96760
Saida Milena Diaz Castillo <63254311+SaidaKIS@users.noreply.github.com>
2021-12-13 18:54:20 +0100

    Accelerated everything!!!!

--- dataset.py
+++ dataset.py
@@ -9,8 +9,6 @@ from scipy.special import softmax
 import time
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-#def weighted_random_choice():
-
 def get_param(degree, size):
     """
     Generate random angle for rotation and define the extension box for define their
@@ -99,43 +97,26 @@ class RandomRotation_crop(torch.nn.Module):
     
       return torch.Tensor(np.array([cr_image_0,cr_image_1]), device='cpu')
 
+class RotationTransform:
+    """Rotate by one of the given angles."""
+
+    def __init__(self, angles):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)
+        return Ttorch.functional.rotate(x, angle)
+
 class SRS_crop(torch.nn.Module):
   def __init__(self, size):
        super().__init__()
        self.size = int(size)
 
-  def forward(self, img, pmap):
+  def forward(self, img, pmap, ind):
       start = time.time()
-      if isinstance(img, Tensor):
-        d_1=img.size(dim=1)
-        d_2=img.size(dim=2)
-      else:
-        raise TypeError("Img should be a Tensor")
-
-      end = time.time()
-      print('1 -> ', end-start)
-      start = end
-
-      ext_1 = [float(self.size), float(d_1-self.size)]
-      ext_2 = [float(self.size), float(d_2-self.size)]
-      
-      cut_pmap = softmax(pmap[int(ext_1[0]): int(ext_1[1]), int(ext_2[0]): int(ext_2[1])])
-      end = time.time()
-      print('2 -> ', end-start)
-      start = end
-
-      ind = np.array(list(np.ndindex(cut_pmap.shape)))
-      end = time.time()
-      print('3 -> ', end-start)
-      start = end
-
-      pos = np.random.choice(np.arange(len(cut_pmap.flatten())), 1, p=cut_pmap.flatten())
-      end = time.time()
-      print('4 -> ', end-start)
-      start = end
-      
-      c = (int(ind[pos[0],1])+int(ext_1[0]), int(ind[pos[0],0])+int(ext_2[0]))
-
+      counter = np.arange(len(pmap))
+      pos = np.random.choice(counter, 1, p=pmap)      
+      c = (int(ind[pos[0],1])+int(self.size/2), int(ind[pos[0],0])+int(self.size/2))
       img_raw=img.cpu().detach().numpy()
 
       x = int(c[0] - self.size/2)
@@ -143,13 +124,10 @@ class SRS_crop(torch.nn.Module):
 
       cr_image_0 = img_raw[0,y:y+self.size, x:x+self.size]
       cr_image_1 = img_raw[1,y:y+self.size, x:x+self.size]
-
       end = time.time()
-      print('5 -> ', end-start)
-      start = end
+      print('1-> ', end-start)
 
       return torch.Tensor(np.array([cr_image_0,cr_image_1]), device='cpu')
-
 
 class Secuential_trasn(torch.nn.Module):
     """Generates a secuential transformation"""
@@ -157,11 +135,11 @@ class Secuential_trasn(torch.nn.Module):
        super().__init__()
        self.transforms = transforms
 
-    def __call__(self, img, pmap):
+    def __call__(self, img, pmap, ind):
       t_list=[img]      
       for t in range(len(self.transforms)):
         if t == 1:
-          rotation = self.transforms[t](t_list[-1], pmap)
+          rotation = self.transforms[t](t_list[-1], pmap, ind)
           t_list.append(rotation)
         else:
           t_list.append(self.transforms[t](t_list[-1]))
@@ -183,14 +161,9 @@ class segDataset(torch.utils.data.Dataset):
     self.bin_classes = ['Intergranular lane', 'Granules with dots', 'Granules with lanes',
                         'Complex-shape granules', 'Normal-shape granules']
 
-    #self.transform_serie = Secuential_trasn([Ttorch.ToTensor(),
-    #                                        RandomRotation_crop((0, 180), self.size),
-    #                                        Ttorch.RandomHorizontalFlip(p=0.5),
-    #                                        Ttorch.RandomVerticalFlip(p=0.5)
-    #                                        ])
-
     self.transform_serie = Secuential_trasn([Ttorch.ToTensor(),
                                             SRS_crop(self.size),
+                                            RotationTransform(angles=[0, 90, 180, 270]),
                                             Ttorch.RandomHorizontalFlip(p=0.5),
                                             Ttorch.RandomVerticalFlip(p=0.5)
                                             ])
@@ -201,37 +174,33 @@ class segDataset(torch.utils.data.Dataset):
     self.smap = []
     self.mask_smap = []
     self.weight_maps = []
+    self.index_list = []
     for f in self.file_list:
       file = np.load(f)
       self.smap.append(file['smap'].astype(np.float32))
       self.mask_smap.append(file['cmask_map'].astype(np.float32))
 
-      weight_maps = np.zeros_like(mask_smap).astype(np.float32)
-      weight_maps[(mask_smap == 0.0)] = 1
-      weight_maps[(mask_smap == 4.0)] = 3
-      weight_maps[(mask_smap == 1.0)] = 6
-      weight_maps[(mask_smap == 2.0)] = 6
-      weight_maps[(mask_smap == 3.0)] = 5
+      pmap = file['cmask_map']
+      weight_maps = np.zeros_like(pmap[self.size:, :-self.size]).astype(np.float32)
+      weight_maps[pmap[self.size:, :-self.size] == 0.0] = 1
+      weight_maps[pmap[self.size:, :-self.size] == 4.0] = 3
+      weight_maps[pmap[self.size:, :-self.size] == 1.0] = 6
+      weight_maps[pmap[self.size:, :-self.size] == 2.0] = 6
+      weight_maps[pmap[self.size:, :-self.size] == 3.0] = 5
 
-      self.weight_maps.append(weight_maps)
-      
+      self.weight_maps.append(softmax(weight_maps).flatten())
+      self.index_list.append(np.array(list(np.ndindex(weight_maps.shape))))
+
   def __getitem__(self, idx):
-    
-    # file_name = random.choice(self.file_list)
-    # file = np.load(file_name)
-    # smap = file['smap'].astype(np.float32)
-    # mask_smap = file['cmask_map'].astype(np.float32)
     
     ind = np.random.randint(low=0, high=len(self.file_list))
     smap = self.smap[ind]
     mask_smap = self.mask_smap[ind]
 
-
     #Full probability maps calculation
     weight_maps = self.weight_maps[ind]
-    
-    
-    img_t = self.transform_serie(np.array([smap, mask_smap]).transpose(), weight_maps)
+    index_l = self.index_list[ind]
+    img_t = self.transform_serie(np.array([smap, mask_smap]).transpose(), weight_maps, index_l)
 
     self.image = img_t[0].unsqueeze(0)
     self.mask = img_t[1].type(torch.int64)
